104bf9de0f260bd394c24f503ec10e228e3cb0cf
Briel <christiamgsp@gmail.com>
2026-03-26 17:31:48 +0100

    Configuración exitosa de Tailwind CSS v4

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { tableroInicial } from './data/datosIniciales';
 
 function App() {
   return (
-    <div>
-      <h1>OrganizeMe</h1>
+    <div className='h-screen bg-slate-900 flex items-center justify-center'>
+      <h1 className='text-white text-3xl font-bold border-b-4 border-emerald-500 pb-2'>
+        OrganizeMe
+      </h1>
       <p>El tablero de mis sueños está en camino...</p>
     </div>
   );
